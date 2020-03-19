22846950c4f10a68a1e590dd9a4f06d4636c6e70
Adam Żurek <adam.zurek@predicagroup.com>
2020-03-19 10:04:32 +0100

    Change the theme back

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import astropy-sphinx-theme
+import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
@@ -29,7 +29,7 @@ author = 'Test'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "astropy-sphinx-theme",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -46,7 +46,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'astropy-sphinx-theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
